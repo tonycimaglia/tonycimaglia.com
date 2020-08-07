aefdafdd41627fcab78599b95316ef110757a87f
tonycimaglia <tonycimaglia@gmail.com>
2020-08-07 14:25:56 -0400

    replaced banner image and modified site desc.

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,7 +48,7 @@ module.exports = {
       options: {
         name: `minimal-blog - @lekoarts/gatsby-theme-minimal-blog`,
         short_name: `minimal-blog`,
-        description: `Typography driven, feature-rich blogging theme with minimal aesthetics. Includes tags/categories support and extensive features for code blocks such as live preview, line numbers, and code highlighting.`,
+        description: `Software engineer writing accessible tutorials for devs`,
         start_url: `/`,
         background_color: `#fff`,
         theme_color: `#6B46C1`,
