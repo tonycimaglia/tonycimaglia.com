b789726ff5e7bf529995ada41a65970885f1da4a
tonycimaglia <tonycimaglia@gmail.com>
2020-09-11 15:53:48 -0400

    started shadowing components to start more customization.

--- src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
+++ src/@lekoarts/gatsby-theme-minimal-blog/components/footer.tsx
@@ -1,9 +1,9 @@
 /** @jsx jsx */
-import {jsx, Link} from 'theme-ui'
-import useSiteMetadata from '../hooks/use-site-metadata'
+import { jsx, Link } from "theme-ui"
+import useSiteMetadata from "../hooks/use-site-metadata"
 
 const Footer = () => {
-  const {siteTitle} = useSiteMetadata()
+  const { siteTitle } = useSiteMetadata()
 
   return (
     <footer
@@ -21,9 +21,22 @@ const Footer = () => {
       }}
     >
       <div>
-        &copy; {new Date().getFullYear()} by {siteTitle}
+        &copy; {new Date().getFullYear()} by {siteTitle}. All rights reserved.
       </div>
-      <div></div>
+      <div>
+        <Link
+          aria-label="Link to the theme's GitHub repository"
+          href="https://github.com/LekoArts/gatsby-themes/tree/master/themes/gatsby-theme-minimal-blog"
+        >
+          Theme
+        </Link>
+        {` `}
+        by
+        {` `}
+        <Link aria-label="Link to the theme author's website" href="https://www.lekoarts.de/en">
+          LekoArts
+        </Link>
+      </div>
     </footer>
   )
 }
